909e7369725c2fdc4a18a4751f738afed1356a20
Travis Berres <tber8@hotmail.com>
2026-05-01 19:59:03 -0400

    Added input actions and player movement

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,6 +1,9 @@
 extends CharacterBody2D
 
 var health = 2
+var move_speed = 2
+
+var _movement_vector: Vector2 = Vector2()
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -9,6 +12,23 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+func _physics_process(delta: float) -> void:
+	_movement_vector = Vector2()
+	
+	if Input.is_action_pressed("MoveUp"):
+		_movement_vector.y -= 1
+	
+	if Input.is_action_pressed("MoveDown"):
+		_movement_vector.y += 1
+	
+	if Input.is_action_pressed("MoveLeft"):
+		_movement_vector.x -= 1
+	
+	if Input.is_action_pressed("MoveRight"):
+		_movement_vector.x += 1
+	
+	position += _movement_vector.normalized()*move_speed
 
 func take_damage(amount: int):
 	health-=amount
